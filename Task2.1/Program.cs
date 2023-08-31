af123db6b9bfd03a1d9f1fc40e8acccd989ef5c7
Olga <workpost.mkh@yandex.ru>
2023-08-31 13:18:10 +0300

    Task 2.1

---
+++ Task2.1/Program.cs
@@ -0,0 +1,35 @@
+﻿//  напишите программу которая принимает на вход число и выводит колличество цифр в числе
+// var1
+/*int InputNum (string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+/int num = InputNum("Введите число: ");
+int i; // i вынесли за цикл потому что нам потом нужно ее распечатать, иначе она бы существовала только внутри цикла и была бе недоступна вне его.
+for (i = 0; num != 0; i++)
+{
+    num /= 10;
+}
+
+Console.WriteLine(i); */
+
+// var2
+
+int InputNum (string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int num = InputNum("Введите число: ");
+int i; 
+for (i = 0; num != 0; i++, num /= 10);
+
+Console.WriteLine(i);
+
+
+
+
+
