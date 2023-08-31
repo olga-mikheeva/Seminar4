a31996f8883f6a99e78c40332a2a48511220d2be
Olga <workpost.mkh@yandex.ru>
2023-08-31 12:14:52 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу которая принимает на вход число и выводит колличество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+while(number != 0)
+{
+    number /= 10;
+    count ++;
+}
+Console.WriteLine(count);
+
