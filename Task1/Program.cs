67252076dd3b666ea73b19a8286d32bc9d04f530
Olga <workpost.mkh@yandex.ru>
2023-08-31 11:50:46 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// напишите программу которая принимает на вход число N и выдает цифры от 1 до N
+
+int InputNum (string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int SumNums (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+      sum += i;  
+    }
+    return sum;
+}
+
+int num = InputNum("Введите число: ");
+int sum = SumNums(num);
